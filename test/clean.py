6a2859f5d86dfeeab5fee3b4f5fb2477852aa1ca
Ritu Mundhe <rmundhe@iastate.edu>
2021-03-20 18:40:46 -0500

    raw text files for 2019 by case id

--- test/clean.py
+++ test/clean.py
@@ -3,29 +3,52 @@
 # a is a tuple (speakers, targets)
 # b is a tuple (speakers, targets)
 import convokit
+import nltk
+from convokit.model import corpusHelper
 from convokit import Utterance, Corpus, Coordination, download
 from convokit.text_processing import TextProcessor
-
+import json
 from convokit.text_processing import TextParser
-ROOT_DIR = "<Convokit-home>/.convokit/downloads/supreme-2019"
-corpus = convokit.Corpus(ROOT_DIR,utterance_end_index=199)
+# ROOT_DIR = download("supreme-2019")
+ROOT_DIR = "/Users/rmundhe/.convokit/downloads/supreme-2019"
+# corpus = convokit.Corpus(ROOT_DIR,utterance_end_index=100)
+corpus = convokit.Corpus(ROOT_DIR)
+# nltk.download('punkt')
 
-
-test_utt_id = '24929__0_007'
 
 def preprocess_text(text):
-    text = text.replace(' -- ', ' ')
+    text = text.replace('--', ' ')
+    text = text.replace('\n', ' ')
     return text
 
 # corpus = Corpus(filename=download("supreme-2019"))
 prep = TextProcessor(proc_fn=preprocess_text, output_field='clean_text')
 corpus = prep.transform(corpus)
-parser = TextParser(input_field='clean_text', verbosity=50)
-corpus = parser.transform(corpus)
+# parser = TextParser(input_field='clean_text', verbosity=50)
+# corpus = parser.transform(corpus)
 
-utt = corpus.get_utterance(test_utt_id)
+utt = corpus.random_utterance()
 print(utt.meta)
 print(utt.retrieve_meta('clean_text'))
+sp = utt.get_speaker().meta
+print(sp["name"])
+# test_parse = utt.retrieve_meta('parsed')
+# print(test_parse[2])
 
-test_parse = utt.retrieve_meta('parsed')
-print(test_parse[0])
+# texttagger = TextParser(output_field='tagged', input_field='clean_text', mode='tag')
+# corpus = texttagger.transform(corpus)
+# print(utt.retrieve_meta('tagged')[0])
+
+for u in corpus.iter_utterances():
+  filename="raw"+u.meta["case_id"]+".txt"
+  f = open(filename, "a")
+  f.write("\n")
+  sp = u.get_speaker().meta
+  if sp["name"] != "":
+    name = sp["name"].replace('.', '')
+    f.write(name)
+    f.write(": ")
+  f.write(u.retrieve_meta('clean_text'))
+
+
+f.close()
