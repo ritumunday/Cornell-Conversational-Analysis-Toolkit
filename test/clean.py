f0ea04a1cbd543a59c91e2b7b2e87b8d44bda3c4
Ritu Mundhe <rmundhe@iastate.edu>
2021-03-21 10:05:23 -0500

    Script to dump txt and json by year

--- test/clean.py
+++ test/clean.py
@@ -1,46 +1,48 @@
-# helper function to plot two coordination scores against each other as a chart,
-#   on aggregate and by coordination marker
-# a is a tuple (speakers, targets)
-# b is a tuple (speakers, targets)
 import convokit
-import nltk
-from convokit.model import corpusHelper
-from convokit import Utterance, Corpus, Coordination, download
 from convokit.text_processing import TextProcessor
 import json
 from convokit.text_processing import TextParser
-# ROOT_DIR = download("supreme-2019")
-ROOT_DIR = "/Users/rmundhe/.convokit/downloads/supreme-2019"
-# corpus = convokit.Corpus(ROOT_DIR,utterance_end_index=100)
-corpus = convokit.Corpus(ROOT_DIR)
-# nltk.download('punkt')
 
+# Your convokit home
+CONVOKIT_HOME= "/Users/rmundhe/.convokit"
+ROOT_DIR = CONVOKIT_HOME + "/downloads/supreme-corpus"
 
+# basic cleanup for raw text file
 def preprocess_text(text):
     text = text.replace('--', ' ')
     text = text.replace('\n', ' ')
     return text
 
-# corpus = Corpus(filename=download("supreme-2019"))
-prep = TextProcessor(proc_fn=preprocess_text, output_field='clean_text')
-corpus = prep.transform(corpus)
-# parser = TextParser(input_field='clean_text', verbosity=50)
-# corpus = parser.transform(corpus)
+textprep = TextProcessor(proc_fn=preprocess_text, output_field='clean_text')
+texttagger = TextParser(output_field='tagged', input_field='clean_text', mode='tag')
 
-utt = corpus.random_utterance()
-print(utt.meta)
-print(utt.retrieve_meta('clean_text'))
-sp = utt.get_speaker().meta
-print(sp["name"])
-# test_parse = utt.retrieve_meta('parsed')
-# print(test_parse[2])
+uttfile = ROOT_DIR + "/utterances.jsonl"
 
-# texttagger = TextParser(output_field='tagged', input_field='clean_text', mode='tag')
-# corpus = texttagger.transform(corpus)
-# print(utt.retrieve_meta('tagged')[0])
+# print("initializing corpus. this may take a minute.")
+# corpus = convokit.Corpus(ROOT_DIR)
+print("initializing sample corpus.")
+corpus = convokit.Corpus(ROOT_DIR, utterance_end_index=500)
+print("corpus initialized")
 
+count = 0
+y = 1955
+# assuming utterance file sorted by year
 for u in corpus.iter_utterances():
-  filename="raw"+u.meta["case_id"]+".txt"
+  count = count + 1
+  year = u.meta["case_id"][0:4]
+  # clean and tag utterance
+  u = textprep.transform_utterance(u)
+  u = texttagger.transform_utterance(u)
+  print("utterance ", year, " ", u.id)
+
+  # save json
+  jsonfile = "data/tagged"+ year +".json"
+  ft=open(jsonfile, "a")
+  ft.write(",")
+  ft.write(json.dumps(u.meta))
+
+  # save text
+  filename="data/raw"+ year +".txt"
   f = open(filename, "a")
   f.write("\n")
   sp = u.get_speaker().meta
@@ -49,6 +51,10 @@ for u in corpus.iter_utterances():
     f.write(name)
     f.write(": ")
   f.write(u.retrieve_meta('clean_text'))
+  if y < int(year):
+    print("finished year ", year)
+    f.close()
+    ft.close()
+    y = year
 
-
-f.close()
+print("utterances processed ", count)
