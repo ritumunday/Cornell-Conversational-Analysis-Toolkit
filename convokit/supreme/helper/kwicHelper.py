b04b39e701fc1133c02ab5844133f0fe8784946d
Ritu Mundhe <rmundhe@iastate.edu>
2021-03-27 12:43:09 -0500

    Added error handling, added columns to kwic and refactored kwic logic

--- convokit/supreme/helper/kwicHelper.py
+++ convokit/supreme/helper/kwicHelper.py
@@ -11,11 +11,18 @@ class KwicHelper:
     """
 
     def __init__(self, corpus, separator, resultfile):
+        self.separator = separator
+        self.ft = open(resultfile, "w")
+        print("========================================================================")
         print("Started processing")
+        print("========================================================================")
         self.corpus = corpus
         self.loadcorpus()
-        self.modalkwic(separator, resultfile)
-        print("Finished processing")
+        self.modalkwic()
+        self.ft.close()
+        print("========================================================================")
+        print("Finished processing. Result file saved in supreme/results folder.")
+        print("========================================================================")
 
     """
     Basic cleanup function for raw sentences
@@ -36,7 +43,6 @@ class KwicHelper:
 
     def loadcorpus(self):
 
-        print("========================================================================")
         print("Cleaning sentences")
         # Clean sentence text
         textprep = TextProcessor(proc_fn=self.prep_text, output_field='clean_text')
@@ -57,60 +63,65 @@ class KwicHelper:
         transformer = ModalSentences(input_field='parsed', output_field='ismodal')
         self.corpus = transformer.transform(self.corpus)
 
+    def printline(self, parsedsent, u, passive, inter, mod, modalindex, verb, passivized):
+        toks = list(map(lambda x: x['tok'], parsedsent["toks"]))
+        firstseg = "" if modalindex == 0 else reduce((lambda x, y: x + " " + y), toks[0:modalindex])
+        lastseg = "" if modalindex == len(toks) - 1 else reduce((lambda x, y: x + " " + y),
+                                                                toks[modalindex + 1:len(toks)])
+        fileline = u.meta[
+                       "year"] + self.separator + u.id + self.separator + firstseg + self.separator + mod.lower() + self.separator + lastseg + self.separator + verb.lower() + self.separator + passivized.lower() + self.separator + str(
+            passive) + self.separator + str(inter) + self.separator + u.speaker.meta[
+                       'type'] + self.separator + u.speaker.meta['name'] + "\n"
+        self.ft.write(fileline)
+
     """
     Searches for modals and main verbs and generates excel file for KWIC analysis. 
     """
 
-    def modalkwic(self, separator, resultfile):
+    def modalkwic(self):
         print("Creating KWIC")
-        ft = open(resultfile, "w")
+
         # header row
-        ft.write(
-            "Year" + separator + "Sentence ID" + separator + "Before" + separator + "Mod" + separator + "After" + separator + "Main Verb" + separator + "Passive" + separator + "Speaker" + separator + "Role" + "\n")
-        print("========================================================================")
-        # assuming utterance file is sorted by year, iterate. Skip all non questions and non modals.
-        for u in self.corpus.iter_utterances(lambda u: u.meta["ismodal"] == 1 and u.meta["questions"] != []):
+        self.ft.write(
+            "Year" + self.separator + "Sentence ID" + self.separator + "Before" + self.separator + "Mod" + self.separator + "After" + self.separator + "Main Verb" + self.separator + "Passivized" + self.separator + "Passive" + self.separator + "Interrogative" + self.separator + "Role" + self.separator + "Speaker\n")
+        # assuming utterance file is sorted by year, iterate. Skip all non modals.
+        for u in self.corpus.iter_utterances(lambda u: u.meta["ismodal"] == 1):
 
             parsedsents = u.meta["parsed"]
-            cleantext = u.meta["clean_text"]
-
+            # Loop through modal sentences
             for parsedsent in parsedsents:
-                # Find first main verb after modal in the sentence, skipping passive auxiliaries
-                if len(parsedsent):
-                    fileline = mod = first = last = ""
-                    unmatchedmodal = passive = i = 0
-                    # sentence
-                    for tokenized in parsedsent["toks"]:
-                        # word
-                        if tokenized["tag"] == "MD":
-                            mod = tokenized["tok"]
-                            toks = list(map(lambda x: x['tok'], parsedsent["toks"]))
-                            # tags = list(map(lambda x: x['tag'], parsedsent[0]["toks"]))
-                            first = "" if i == 0 else reduce((lambda x, y: x + " " + y), toks[0:i])
-                            last = "" if i == len(toks) - 1 else reduce((lambda x, y: x + " " + y),
-                                                                        toks[i + 1:len(toks)])
-                            unmatchedmodal = 1
+                try:
+                    # Find first main verb after modal in the sentence, skipping passive auxiliaries
+                    if len(parsedsent):
+                        modalindex = passive = unmatchedmodal = i = 0
+                        auxpass = mod = passivized = ""
 
-                        # Skip aux passive verb
-                        if tokenized["tag"] == "VB" and tokenized["dep"] == "auxpass":
-                            passive = 1
-                            print("Passive detected:", cleantext)
+                        inter = 0 if u.meta["questions"] == [] else 1
+                        # sentence
+                        for tokenized in parsedsent["toks"]:
+                            # word
+                            if tokenized["tag"] == "MD":
+                                mod = tokenized["tok"]
+                                unmatchedmodal = 1
+                                modalindex = i
 
-                        # Find modal main verb
-                        if ((tokenized["tag"] == "VB" and passive == 0) or
-                            ((passive == 1) and (tokenized["tag"] == "VBN"))) and unmatchedmodal == 1:
-                            vb = tokenized["tok"]
-                            fileline = u.meta[
-                                           "year"] + separator + u.id + separator + first + separator + mod.lower() + separator + last + separator + vb.lower() + separator + str(
-                                passive) + separator + \
-                                       u.speaker.meta['name'] + separator + u.speaker.meta['type'] + "\n"
-                            ft.write(fileline)
-                            fileline = mod = first = last = ""
-                            passive = 0
-                            unmatchedmodal = 0
+                            # Skip aux passive verb, list passive terms
+                            if tokenized["tag"] == "VB" and tokenized["dep"] == "auxpass":
+                                passive = 1
+                                auxpass = tokenized["tok"]
 
-                        i = i + 1
-        print("========================================================================")
-        print("Result file saved in supreme/results folder.")
-        print("========================================================================")
-        ft.close()
+                            # Find modal main verb
+                            if ((tokenized["tag"] == "VB" and passive == 0) or
+                                ((passive == 1) and (tokenized["tag"] == "VBN"))) and unmatchedmodal == 1:
+                                verb = tokenized["tok"]
+                                if passive == 1:
+                                    passivized = mod + "  " + auxpass + "  " + verb
+                                # Found main verb, print row
+                                self.printline(parsedsent, u, passive, inter, mod, modalindex, verb, passivized)
+
+                                auxpass = mod = passivized = ""
+                                modalindex = inter = passive = unmatchedmodal = 0
+                            i = i + 1
+                    print("Processed line ", u.id)
+                except Exception as e:
+                    print("Exception on line ", u.id, ":", e)
